9e53537067b3a2b17a967be82b0659aee65082ee
vikas-0512 <velmavikas5123@gmail.com>
2023-05-30 07:37:53 +0530

    new routes

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -14,9 +14,13 @@ migrate=Migrate(app,db)
 
 from routers.userRouter import user_pb
 from routers.teamRouter import team_rt
+from routers.memberRouter import member_rt
+from routers.pollRouter import poll_rt
 
 app.register_blueprint(team_rt,url_prefix="/teams")
 app.register_blueprint(user_pb,url_prefix="/users")
+app.register_blueprint(member_rt,url_prefix="/member")
+app.register_blueprint(poll_rt,url_prefix="/poll")
 
 if __name__=='__main__':
     app.run(debug=True)
